9655ae5f4c0fed819d5c4dcfafe78aa748ae5bd2
markcompsci <>
2021-06-08 13:33:21 +0000

    linter errors

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const INPUT = document.getElementById("input")
-const BTN = document.getElementById("guess-btn")
+const INPUT = document.getElementById('input')
+const BTN = document.getElementById('guess-btn')
 
-BTN.addEventListener("click", () => {
- const ANSWER = Math.floor(Math.random() * 6) + 1 
- console.log(ANSWER)
- if (+INPUT.value === ANSWER) {
-   alert("Good job, you got it right!")
- } else {
-   alert("Sorry, that was incorrect.")
- }
+BTN.addEventListener('click', () => {
+   const ANSWER = Math.floor(Math.random() * 6) + 1
+   if (+INPUT.value === ANSWER) {
+    alert('Good job, you got it right!')
+  } else {
+    alert('Sorry, that was incorrect.')
+  }
 })
